fc119dfd35e94523c52f798bd716add11d265885
Karel Martyns <martynskarel@gmail.com>
2026-06-11 23:55:44 +0300

    Things added

--- Dental.API/Controllers/PatientsController.cs
+++ Dental.API/Controllers/PatientsController.cs
@@ -5,7 +5,27 @@ using Dental.API.Data;
 
 namespace Dental.API.Controllers
 {
-    public class PatientsController
+    /*
+     * i have used controllers before, so before writing the code i must first explain waht controllers are, why they are necessary for this project structure, and 
+     * then waht methods we will need to write then start immediately.
+     * All the nots will be written, first lets write the code
+     */
+    [ApiController]
+    [Route("Api/[controller]")]
+
+    public class PatientsController : ControllerBase
     {
+        // we need to inject the database context to access the databse
+        private readonly AppDbContext _appDbContext; // need to go and write the AppDbContext class first so that this line works
+
+        public PatientsController(AppDbContext appDbContext)
+        {
+            appDbContext = _appDbContext;
+        }
+      
     }
+
 }
+
+
+
